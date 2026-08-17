e39875439c4e715b07d1cea472c9d8e0783553fe
Allsimon <simon.allegraud@gmail.com>
2026-08-17 21:59:53 +0200

    chore: verify that the gradle wrapper is up-to-date

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,7 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+{
+
+  imports = [ ./devenv-test.nix ];
 
   languages.java = {
     enable = true;
@@ -8,24 +11,6 @@
     lsp.enable = false;
   };
   services.nginx.enable = true;
-
-  # Keep the Gradle wrapper on the same Gradle as the shell and the packaged
-  # build. 'copy' mode because the wrapper tasks rewrite this file in place,
-  # while devenv stays the source of truth for its contents.
-  files."gradle/wrapper/gradle-wrapper.properties" = {
-    copyMode = "copy";
-    text = ''
-      distributionBase=GRADLE_USER_HOME
-      distributionPath=wrapper/dists
-      distributionUrl=https\://services.gradle.org/distributions/gradle-${config.languages.java.gradle.package.version}-bin.zip
-      networkTimeout=10000
-      retries=0
-      retryBackOffMs=500
-      validateDistributionUrl=true
-      zipStoreBase=GRADLE_USER_HOME
-      zipStorePath=wrapper/dists
-    '';
-  };
 
   treefmt = {
     enable = true;
